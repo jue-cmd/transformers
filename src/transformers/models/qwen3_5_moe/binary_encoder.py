c227b7bc07f1b2d584ef13fcdee86947b20cde83
jue <2796869135@qq.com>
2026-05-31 12:41:11 +0800

    修改了卷积位置编码让他不爆显存

--- src/transformers/models/qwen3_5_moe/binary_encoder.py
+++ src/transformers/models/qwen3_5_moe/binary_encoder.py
@@ -125,6 +125,9 @@ class BytePositionalConv(nn.Module):
             nn.init.zeros_(self.bias)
 
     def forward(self, x, eval_chunk_size=1024*1024):
+        self.weight.data = self.weight.data.to(dtype=x.dtype, device=x.device)
+        if self.bias is not None:
+            self.bias.data = self.bias.data.to(dtype=x.dtype, device=x.device)
         B, L, E = x.shape
         x_pad = torch.nn.functional.pad(x, (0, 0, self.padding_size, self.padding_size), mode='constant', value=0)
         x_windows = x_pad.unfold(dimension=1, size=self.kernel_size, step=1)  # [B, L, E, 5]
